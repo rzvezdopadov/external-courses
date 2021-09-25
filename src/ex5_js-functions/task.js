c751c4a64ebdbd1b3a33e5422b3bba40c29b5c81
rzvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2021-09-25 17:34:53 +0300

    tasks from block 5 have been updated, syntax and decor have been corrected

--- src/ex5_js-functions/task.js
+++ src/ex5_js-functions/task.js
@@ -3,49 +3,73 @@ const Calculator = () => {
 
   const add = (a = 0) => {
     result += a;
+
     let c;
+
     const f = (b = 0) => {
       if (b !== undefined) { result += b; }
+
       c = b;
+
       return f;
     };
+
     if (c === undefined) return add;
+
     return f;
   };
 
   const subtract = (a = 0) => {
     result -= a;
+
     let c;
+
     const f = (b) => {
       if (b !== undefined) { result -= b; }
+
       c = b;
+
       return f;
     };
+
     if (c === undefined) return subtract;
+
     return f;
   };
 
   const divide = (a = 1) => {
     result /= a;
+
     let c;
+
     const f = (b) => {
       if (b !== undefined) { result /= b; }
+
       c = b;
+
       return f;
     };
+
     if (c === undefined) return divide;
+
     return f;
   };
 
   const multiply = (a = 1) => {
     result *= a;
+
     let c;
+
     const f = (b) => {
       if (b !== undefined) { result *= b; }
+
       c = b;
+
       return f;
     };
+
     if (c === undefined) return multiply;
+
     return f;
   };
 
